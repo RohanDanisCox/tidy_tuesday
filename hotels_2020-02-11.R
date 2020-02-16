92afa908f824a5b99615997a935aaed02a2dd4c4
Rohan Home Comp <rohan.daniscox@gmail.com>
2020-02-16 17:03:01 +1100

    Investigated variables

--- hotels_2020-02-11.R
+++ hotels_2020-02-11.R
@@ -1,5 +1,7 @@
 # Hotels 2020-02-11
 
+library(tidyverse)
+
 ### 0. Import Data          ####
 
 tuesdata <- tidytuesdayR::tt_load('2020-02-11')
@@ -7,6 +9,14 @@ tuesdata <- tidytuesdayR::tt_load('2020-02-11')
 hotels <- tuesdata$hotels
 
 ### 1. Data Understanding   ####
+
+glimpse(hotels)
+
+hotels_variables <- hotels %>% 
+  map_df(~(data.frame(n_distinct = n_distinct(.x),
+                      class = class(.x),
+                      na_count = sum(is.na(.x)))),
+         .id = "variable")
 
 ### 2. Data Preparation     ####
 
